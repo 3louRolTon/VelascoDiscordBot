8ab89f0bba049083509ccab2493324c481fca500
Yury <Yury@DESKTOP-0S4RF45>
2021-10-21 01:22:52 +0300

    Добавьте файлы проекта.

---
+++ VelascoDiscordBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace VelascoDiscordBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, Token.token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ VelascoDiscordBot/Token.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VelascoDiscordBot
+{
+    class Token
+    {
+        public static string token = "NDc1NjEyMjIxODM0MDAyNDQy.W2bSZg.yvYvjOgUI8EL_-6Wl0Q-ZnwfUhU";
+    }
+}
