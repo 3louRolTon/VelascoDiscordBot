2d5a54b81f7fb8f4dd5dc425036ef847c27ba6e5
3louRoolTon <skrobat.yura@gmail.com>
2021-10-22 00:04:39 +0300

    Initialize bot

--- VelascoDiscordBot/Program.cs
+++ VelascoDiscordBot/Program.cs
@@ -1,33 +1,10 @@
-﻿using Discord;
-using Discord.WebSocket;
-using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace VelascoDiscordBot
 {
     class Program
     {
-        private DiscordSocketClient _client;
-
-        public static void Main(string[] args)
-            => new Program().MainAsync().GetAwaiter().GetResult();
-
-        public async Task MainAsync()
-        {
-            _client = new DiscordSocketClient();
-
-            _client.Log += Log;
-
-            await _client.LoginAsync(TokenType.Bot, Token.token);
-            await _client.StartAsync();
-
-            await Task.Delay(-1);
-        }
-
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
+        static async Task Main(string[] args)
+            => await new VelascoBotClient().InitializeAsync();
     }
 }
---
+++ VelascoDiscordBot/VelascoBotClient.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using VelascoDiscordBot.Services;
+
+namespace VelascoDiscordBot
+{
+    class VelascoBotClient
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _cmdService;
+        private readonly LogService _logService;
+
+        public VelascoBotClient()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                AlwaysDownloadUsers = true,
+                MessageCacheSize = 50,
+                LogLevel = LogSeverity.Debug
+            });
+
+            _logService = new LogService();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _client.LoginAsync(TokenType.Bot, Token.Key);
+            await _client.StartAsync();
+            _client.Log += LogAsync;
+
+            await Task.Delay(-1);
+        }
+
+        private async Task LogAsync(LogMessage logMessage)
+        {
+            await _logService.LogAsync(logMessage);
+        }
+    }
+}
